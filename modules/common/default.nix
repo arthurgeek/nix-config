c03572ccaeb7810e036af6410ebaa6080b73f588
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2026-03-08 01:20:08 -0300

    feat(nix): add automatic garbage collection
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -4,6 +4,10 @@
   fonts.packages = with pkgs; [ fira-code nerd-fonts.fira-code ];
 
   nix.settings.experimental-features = "nix-command flakes";
+  nix.gc = {
+    automatic = true;
+    options = "--delete-older-than 7d";
+  };
 
   programs.fish.enable = true;
   environment.shells = [ pkgs.fish ];
