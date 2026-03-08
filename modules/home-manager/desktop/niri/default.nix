9b30ebdc6568592d798ff61700ac1aa938e5d851
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2026-03-08 01:19:56 -0300

    feat(noctalia): add plugins and lock screen keybind
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

--- modules/home-manager/desktop/niri/default.nix
+++ modules/home-manager/desktop/niri/default.nix
@@ -10,6 +10,72 @@
     programs.noctalia-shell = {
       enable = true;
       settings = { };
+      plugins = {
+        sources = [
+          {
+            enabled = true;
+            name = "Official Noctalia Plugins";
+            url = "https://github.com/noctalia-dev/noctalia-plugins";
+          }
+        ];
+        states = {
+          privacy-indicator = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          screen-recorder = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          clipper = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          polkit-agent = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          neovim-session-provider = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          file-search = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          weather-indicator = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+          screenshot = {
+            enabled = true;
+            sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+          };
+        };
+        version = 1;
+      };
+      pluginSettings = {
+        privacy-indicator = {
+          hideInactive = true;
+          enableToast = false;
+          iconSpacing = 4;
+          removeMargins = true;
+          activeColor = "error";
+          inactiveColor = "none";
+        };
+        screen-recorder = {
+          audioCodec = "opus";
+          audioSource = "none";
+          copyToClipboard = false;
+          filenamePattern = "recording_yyyyMMdd_HHmmss";
+          frameRate = "60";
+          hideInactive = true;
+          quality = "very_high";
+          showCursor = true;
+          videoCodec = "h264";
+          videoSource = "portal";
+        };
+      };
     };
 
     xdg.configFile."niri/config.kdl".source = ./config.kdl;
